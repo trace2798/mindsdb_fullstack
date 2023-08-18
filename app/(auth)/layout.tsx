bdaa5cb2c9818c80349a0430b60e8af69d16cc4d
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-08-18 16:10:23 +0530

    made auth page in the center

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
     children: React.ReactNode
   }) {
     return (
-      <div className="flex items-center justify-center h-full w-full">
+      <div className="flex items-center justify-center w-full min-h-screen">
         {children}
       </div>
     );
